188a57c77572f87d3c1ec12e200050e57b4f9a63
Nkululeko Sibeko <112828662+nkululekosibeko@users.noreply.github.com>
2024-03-17 00:44:58 +0200

    Changed the hashing method.
    indicates that Flask-WTF is unable to recognize the hash method 'sha256'.
    
    So I had to explicitly specify the hash method, Instead of relying on the default hashing method.

--- website/auth.py
+++ website/auth.py
@@ -53,7 +53,7 @@ def signup():
             return redirect(url_for('auth.signup'))
 
         # Creating a new user
-        new_user = User(email=email, password=generate_password_hash(password, method='sha256'))
+        new_user = User(email=email, password=generate_password_hash(password, method='pbkdf2:sha256'))
         db.session.add(new_user)
         db.session.commit()
 
