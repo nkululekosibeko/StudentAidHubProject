4bd5ed37ae69a940150794898b00fb6838ba374d
Nkululeko Sibeko <112828662+nkululekosibeko@users.noreply.github.com>
2024-03-17 19:45:00 +0200

    Updated admin_dashoard
    This was done to fit the parameters of the information to be utilized in this system.

--- website/models.py
+++ website/models.py
@@ -11,6 +11,25 @@ class User(db.Model, UserMixin):
     email = db.Column(db.String(50), unique=True, nullable=False)
     password = db.Column(db.String(100), nullable=False)
 
+class FieldOfStudy(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False, unique=True)
+
+class Bursaries(db.Model):
+    bursary_id = db.Column(db.String(12), primary_key=True)
+    field_of_study_id = db.Column(db.Integer, db.ForeignKey('field_of_study.id'), nullable=False)
+    field_of_study = db.relationship('FieldOfStudy', backref=db.backref('bursaries', lazy=True))
+    bursary_name = db.Column(db.String(250), nullable=False)
+    closing_date = db.Column(db.Date, nullable=False)
+    application_link = db.Column(db.String(250), nullable=False)
+    about_sponsor = db.Column(db.String(100000), nullable=False)
+    about_bursary = db.Column(db.String(100000), nullable=False)
+    eligibility_criteria = db.Column(db.String(100000), nullable=False)
+    to_be_covered = db.Column(db.String(100000), nullable=False)
+    documents_required = db.Column(db.String(100000), nullable=False)
+
+
+
     # Implement required methods for Flask-Login
     def is_authenticated(self):
         return True
