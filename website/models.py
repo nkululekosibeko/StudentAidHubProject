0e8901d6f872a1292d6ca78318fe6c600ced623b
Nkululeko Sibeko <112828662+nkululekosibeko@users.noreply.github.com>
2024-03-17 16:03:28 +0200

    Updated models.py
    With these changes, I believe Flask-Login should now be properly integrated into my application, and I shouldn't encounter the "AttributeError" anymore.
    
    Made sure I imported the necessary modules and classes at the top of each file. Also, ensured my user model implements the required methods for Flask-Login to work correctly.

--- website/models.py
+++ website/models.py
@@ -2,10 +2,26 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
-class User(db.Model):
+from flask_login import UserMixin
+
+class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     first_name = db.Column(db.String(100), nullable=False)
     last_name = db.Column(db.String(100), nullable=False)
     email = db.Column(db.String(50), unique=True, nullable=False)
     password = db.Column(db.String(100), nullable=False)
+
+    # Implement required methods for Flask-Login
+    def is_authenticated(self):
+        return True
+
+    def is_active(self):
+        return True
+
+    def is_anonymous(self):
+        return False
+
+    def get_id(self):
+        return str(self.id)
+
 
